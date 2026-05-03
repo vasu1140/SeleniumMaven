33bcaffee5ccdde470904312e662b6b729d87cd4
vasu1140 <musuku.vasu.reddy@gmail.com>
2026-05-03 15:04:30 +0530

    Many mains in extra class
    Many mains in extra class

--- src/main/java/Classes/ExtraClass.java
+++ src/main/java/Classes/ExtraClass.java
@@ -7,4 +7,15 @@ public class ExtraClass {
 
 	}
 
+	public static void main1(String[] args) {
+		System.out.println("Extra Class");
+
+	}
+
+	public static void main2(String[] args) {
+		System.out.println("Extra Class");
+
+	}
+
 }
+
